584ee8d02f9a821916fbf1d858d1cfdc066934e1
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-11-08 01:40:16 +0500

    adding run script

---
+++ run.sh
@@ -0,0 +1,16 @@
+cd apiGatewayServer
+mvn clean package -DskipTests
+cd ../chatClientService
+mvn clean package -DskipTests
+cd ../eurekaNamingServer
+mvn clean package -DskipTests
+cd ../messageService
+mvn clean package -DskipTests
+cd ../notificationService
+mvn clean package -DskipTests
+cd ../sessionService
+mvn clean package -DskipTests
+cd ../userService
+mvn clean package -DskipTests
+cd ../
+sudo docker-compose up --build
