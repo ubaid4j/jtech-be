d75c68de711e2f80550d4018733fcb526063c9e6
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-01-29 17:14:51 +0500

    all working

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 cd apiGatewayServer
 sudo chmod +x mvnw
 sudo mvn clean package -DskipTests
-# cd ../chatClientService
-# sudo chmod +x mvnw
-# sudo mvn clean package -DskipTests
+cd ../chatClientService
+sudo chmod +x mvnw
+sudo mvn clean package -DskipTests
 cd ../eurekaNamingServer
 sudo chmod +x mvnw
 sudo mvn clean package -DskipTests
-# cd ../messageService
-# sudo chmod +x mvnw
-# sudo mvn clean package -DskipTests
-# cd ../notificationService
-# sudo chmod +x mvnw
-# sudo mvn clean package -DskipTests
-# cd ../sessionService
-# sudo chmod +x mvnw
-# sudo mvn clean package -DskipTests
-# cd ../userService
-# sudo chmod +x mvnw
-# sudo mvn clean package -DskipTests
+cd ../messageService
+sudo chmod +x mvnw
+sudo mvn clean package -DskipTests
+cd ../notificationService
+sudo chmod +x mvnw
+sudo mvn clean package -DskipTests
+cd ../sessionService
+sudo chmod +x mvnw
+sudo mvn clean package -DskipTests
+cd ../userService
+sudo chmod +x mvnw
+sudo mvn clean package -DskipTests
 cd ../
 sudo docker-compose up --build
